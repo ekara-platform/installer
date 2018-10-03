6912b67d3c0f7f04d9d210df4057520ad89d720e
nirekin <nirekin@hotmail.com>
2018-10-03 09:26:37 +0200

    Remove intermediate compilations [ci skip]

--- copyAllDep.sh
+++ copyAllDep.sh
@@ -1,18 +1,19 @@
 cd ../model
-go build
+#go build
 
 # copydep in  "engine"
 cd ../engine
 ./copydep.sh
-go build
+#go build
 
 # copydep in  "installer"
 cd ../installer
 ./copydep.sh
-go build
+#go build
 
 # copydep in  "installer_image"
 cd ../installer_image
 ./copydep.sh
 cd ./go
+echo "Compilation of the image sources..."
 go build
