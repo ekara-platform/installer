9e05de74dbd24105c24b36b82b7dca85818cc4c1
Adrien LAUER <adrien.lauer@gmail.com>
2019-05-21 17:52:15 +0200

    Update to Ansible 2.8

--- build.sh
+++ build.sh
@@ -3,16 +3,13 @@
 #!/bin/sh
 docker run --rm -v "$PWD/go":/go/src/installer -w /go/src/installer iron/go:dev go build -o installer
 
-echo http_proxy \$1:  $1
-echo https_proxy \$2:  $2
-
-
-
-
 if [ "$1" = "" ]
 then
     docker build -t ekaraplatform/installer .     
 else
+	echo http_proxy \$1:  $1
+	echo https_proxy \$2:  $2
+
 	if [ "$2" = "" ]
 	then
 		echo "   using only http_proxy..."
